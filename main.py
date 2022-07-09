2002bb38bf230ed345033e74a74ed367bc9afa42
Tomas Tazar <tomas.tazar1337@gmail.com>
2022-07-10 01:48:18 +0200

    upd

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-import string
-import numpy;
-
 def main():
     constArr = []
     validConstellation = False
     
     nameofConst = str(input("Please enter the full name of the constellation or the respective abbreviation: "))
-    with open('BSC5.dat', 'r') as data:
+    with open('bsc5.dat', 'r') as data:
         linedata = data.readlines()
-        for i in linedata:
-            constArr.append(i)
+        for line in linedata:
+            constArr.append(line)
         if nameofConst in constArr:
-            print("Valid constellation!")
             validConstellation = True
         else:
             validConstellation = False
     data.close()
-    print(constArr)
 
     if validConstellation == True:
-        return nameofConst
+        indexMatch = constArr.index(nameofConst)
+        
+
     else:
         print("No contellation parsed.")
 main()
-
-
-
-    
