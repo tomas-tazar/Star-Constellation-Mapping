076da0d1a53b5cd2f72b8949c04de7c13c976223
Tomas Tazar <tomas.tazar1337@gmail.com>
2022-07-11 18:41:53 +0200

    upd

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ class Star:
         self.asc = asc
         self.declination = declination
 
+    def calculateCoordinates(self, asc0, declination0):
+        deltaAsc = self.asc - asc0
+        self.x = math.cos(self.declination) * math.sin(deltaAsc)
+        self.y = math.sin(self.declination) * math.cos(declination0) - math.cos(self.declination) * math.cos(deltaAsc) * math.sin(declination0)
+
+        return self.x, self.y
+
 nameofConst = str(input("Please enter the full name of the constellation or the respective abbreviation: "))
 with open('bsc5.dat', 'r') as data:
     for linedata in data.readlines():
@@ -22,16 +29,26 @@ with open('bsc5.dat', 'r') as data:
             magnitude = float(linedata[102:107])
         except:
             ValueError
-            print("magnitude not present, star will be invalid!")
+            print("magnitude is not a valid value, star will be invalid!")
             continue
 
-        asc = [float(x) for x in linedata[75:83]]
-        declination = [float(i) for i in linedata[83:90]]
+        ascHour, ascMin, ascSec = [float(x) for x in (linedata[75:77], linedata[77:79], linedata[79:83])]
+        declinationHour, declinationMin, declinationSec = [float(i) for i in (linedata[83:86], linedata[86:88], linedata[88:90])]
+
+        asc = math.radians((ascHour + ascMin/60 + ascSec/3600) * 15.)
+        declination = math.radians(declinationHour + declinationMin/60 + declinationSec/3600)
 
         starArr.append(Star(name,magnitude,asc,declination))
 
-lenChecker = len(starArr)
-if lenChecker == 0:
+lenOfStarArray = len(starArr)
+
+sumAscension = sum([Star.asc for Star in starArr])
+sumDeclination = sum([Star.declination for Star in starArr])
+asc0 = sumAscension / lenOfStarArray
+declination0 = sumDeclination / lenOfStarArray
+
+lenOfStarArray = len(starArr)
+if lenOfStarArray == 0:
     print("There were no stars of your input found in the Yale Bright Star Catalog.")
 else:
     print('Returning star objects: \n', starArr)
